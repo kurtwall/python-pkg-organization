6187f2f9bf89db2ccd594265b44c7375d57ffdcc
Kurt.Wall <kurt.wall@surescripts.com>
2023-11-15 11:43:19 -0800

    Initial check-in

---
+++ demo_reader/multireader.py
@@ -0,0 +1,24 @@
+# demo_reader/multireader.py
+
+import os
+
+from .compressed import bzipped, gzipped
+
+extension_map = {
+    ".bz2": bzipped.opener,
+    ".gz": gzipped.opener,
+}
+
+
+class MultiReader:
+    def __init__(self, filename):
+        extension = os.path.splitext(filename)[1]
+        opener = extension_map.get(extension)
+        self.filename = filename
+        self.f = opener(filename, "wt")
+
+    def close(self):
+        self.f.close()
+
+    def read(self):
+        return self.f.read()
